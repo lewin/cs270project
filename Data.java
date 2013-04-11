6ef8a14418fe6ad6beefc467a4c71bf4a0306b90
Allen Xiao <xiao.allen@berkeley.edu>
2013-04-10 20:50:27 -0700

    package

--- Data.java
+++ Data.java
@@ -8,38 +8,33 @@
  * Populate these classes using the Gson library
  */
 
+package data;
+
 /**
- * Data management class.
+ * Array of all course names.
+ * Each index corresponds exactly to the index in any of the courses[].
  */
-public static class Data {
+public static String[] courseNames;
 
-    /**
-     * Array of all course names.
-     * Each index corresponds exactly to the index in any of the courses[].
-     */
-    public static String[] courseNames;
+/**
+ * Stores data about the preferences of an individual tutor.
+ */
+public class Tutor {
+    public int tid;
+    public String name;
+    public int[] slots; // ??? [0,2] on preference
+    public int[] courses; // ??? [0,2] on preference
+    public int adjacentPref; // ??? [0,2] on preference
+}
 
-    /**
-     * Stores data about the preferences of an individual tutor.
-     */
-    public class Tutor {
-        public int tid;
-        public String name;
-        public int[] slots; // ??? [0,2] on preference
-        public int[] courses; // ??? [0,2] on preference
-        public int adjacentPref; // ??? [0,2] on preference
-    }
-
-    /**
-     * Stores data about the preferences of an individual office hour slot.
-     */
-    public class Slot {
-        public int sid;
-        public String name; // probably "OFFICE=START-END" or something
-        // ??? something like: 0=wrong office, 1=class in session, 2=other
-        public int[] courses; // ??? [0,2] on preference
-    }
-
+/**
+ * Stores data about the preferences of an individual office hour slot.
+ */
+public class Slot {
+    public int sid;
+    public String name; // probably "OFFICE=START-END" or something
+    // ??? something like: 0=wrong office, 1=class in session, 2=other
+    public int[] courses; // ??? [0,2] on preference
 }
 
 /**
